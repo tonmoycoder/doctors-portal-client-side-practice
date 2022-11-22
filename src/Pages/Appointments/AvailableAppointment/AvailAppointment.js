0772f5c0c5bb7bc16f77be5ce71ad2c9b3b272eb
tonmoy <tanverislamtonmoy2929@gmail.com>
2022-11-22 22:54:39 +0600

    modal done

--- src/Pages/Appointments/AvailableAppointment/AvailAppointment.js
+++ src/Pages/Appointments/AvailableAppointment/AvailAppointment.js
@@ -1,41 +1,60 @@
+import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import Loader from '../../Shared/Loader';
 import AppointmentOption from '../Appointment/AppointmentOption';
 import BookingModal from '../BookingModal/BookingModal';
 
 const AvailAppointment = ({ selectedDate }) => {
-    const [appointmentOptions, setAppointmentOptions] = useState([]);
-    const [treatment, setTreatment] = useState(null);
+  // const [appointmentOptions, setAppointmentOptions] = useState([]);
+  const [treatment, setTreatment] = useState(null);
 
-    useEffect(() => {
-        fetch('appointmentOptions.json')
-            .then(res => res.json())
-            .then(data => setAppointmentOptions(data))
-    }, [])
-    
+  const date = format(selectedDate, 'PP')
 
-    return (
-        <section className='my-16 max-w-7xl mx-auto'>
-            <p className='text-center text-secondary font-bold'>Available Appointments on {selectedDate ? format(selectedDate, 'PP') : '00'}</p>
-            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
-                {
-                    appointmentOptions.map(option => <AppointmentOption
-                        key={option._id}
-                        appointmentOption={option}
-                        setTreatment={setTreatment}
-                    ></AppointmentOption>)
-                }
-            </div>
-            {
-                treatment &&
-                <BookingModal
-                    selectedDate={selectedDate}
-                    treatment={treatment}
-                    setTreatment={setTreatment}
-                ></BookingModal>
-            }
-        </section>
-    );
+  const { data: appointmentOptions = [], refetch ,isLoading } = useQuery({
+    queryKey: ['appointmentOptions',date],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/appointmentOptions?date=${date}`);
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if(isLoading){
+    return <Loader></Loader>
+  }
+
+  // useEffect(() => {
+  //     fetch('http://localhost:5000/appointmentOptions')
+  //         .then(res => res.json())
+  //         .then(data => setAppointmentOptions(data))
+  // }, [])
+
+
+
+  return (
+    <section className="my-16 max-w-7xl mx-auto">
+      <p className="text-center text-secondary font-bold">
+        Available Appointments on {selectedDate ? format(selectedDate, 'PP') : '00:00'}
+      </p>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6">
+        {appointmentOptions.map((option) => (
+          <AppointmentOption
+            key={option._id}
+            appointmentOption={option}
+            setTreatment={setTreatment}></AppointmentOption>
+        ))}
+      </div>
+      {treatment && (
+        <BookingModal
+          selectedDate={selectedDate}
+          treatment={treatment}
+          setTreatment={setTreatment}
+          refetch={refetch}
+          ></BookingModal>
+      )}
+    </section>
+  );
 };
 
 export default AvailAppointment;
